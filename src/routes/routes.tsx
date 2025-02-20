6376f37afa5805856be450e61c375642affce044
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-20 23:47:41 +0600

    Set up React Router with routes for Home, About, Contact, Login, and Register pages

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,30 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const router = createBrowserRouter([
   {
-    path: "/home",
+    path: "/",
     element: <App></App>,
+  },
+  {
+    path: "/about",
+    element: <About></About>,
+  },
+  {
+    path: "/contact",
+    element: <Contact></Contact>,
+  },
+  {
+    path: "/login",
+    element: <Login></Login>,
+  },
+  {
+    path: "/register",
+    element: <Register></Register>,
   },
 ]);
 
