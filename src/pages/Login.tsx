1f40c60a3adb10f1b513cfad0b6a649b603560e3
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-22 13:43:59 +0600

    added jwt-decode

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,28 @@
 import { Button } from "antd";
 import { useForm } from "react-hook-form";
 import { useLoginMutation } from "../redux/features/auth/authApi";
+import { useAppDispatch } from "../redux/features/hooks";
+import { setUser } from "../redux/features/auth/authSlice";
 
 const Login = () => {
+  const dispatch = useAppDispatch();
+
   const { register, handleSubmit } = useForm({
     defaultValues: {
       userId: "A-0001",
       password: "admin123",
     },
   });
-  const [login, { data, error }] = useLoginMutation();
+  const [login, { error }] = useLoginMutation();
 
-  console.log("data==>", data);
-  console.log("error==>", error);
-
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const userInfo = {
       id: data.userId,
       password: data.password,
     };
-    login(userInfo);
+
+    const res = await login(userInfo).unwrap();
+    dispatch(setUser({ user: {}, token: res.data.accessToken }));
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
