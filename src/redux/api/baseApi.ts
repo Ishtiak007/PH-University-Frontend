e0d3429eb2f1c15902940647ed66240cd17e9da8
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-22 13:29:17 +0600

    set cookies in the browser

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,6 +4,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3000/api/v1",
+    credentials: "include",
   }),
   endpoints: () => ({}),
 });
