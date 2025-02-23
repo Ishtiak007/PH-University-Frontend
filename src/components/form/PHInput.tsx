6fd932c41d718af72d22a7ea75ea7f90045f0cd7
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-23 15:59:34 +0600

    resolve type errors

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,7 +1,13 @@
 import { Input } from "antd";
 import { Controller } from "react-hook-form";
 
-const PHInput = ({ type, name, label }) => {
+type TInpurProps = {
+  type: string;
+  name: string;
+  label?: string;
+};
+
+const PHInput = ({ type, name, label }: TInpurProps) => {
   return (
     <div style={{ marginBottom: "20px" }}>
       {label ? label : null}
