2e21be9bf0e5051f22e12254fa634ad419608a3b
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-23 14:33:13 +0600

    create reusable form wrapper component

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,6 +7,7 @@ import { setUser, TUser } from "../redux/features/auth/authSlice";
 import { verifyToken } from "../utils/verifyToken";
 import { useNavigate } from "react-router-dom";
 import { toast } from "sonner";
+import PHForm from "../components/form/PHForm";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -21,29 +22,30 @@ const Login = () => {
   const [login] = useLoginMutation();
 
   const onSubmit = async (data: FieldValues) => {
-    const toastId = toast.loading("Logging in");
+    console.log(data);
+    // const toastId = toast.loading("Logging in");
 
-    try {
-      const userInfo = {
-        id: data.userId,
-        password: data.password,
-      };
+    // try {
+    //   const userInfo = {
+    //     id: data.userId,
+    //     password: data.password,
+    //   };
 
-      const res = await login(userInfo).unwrap();
+    //   const res = await login(userInfo).unwrap();
 
-      const user = verifyToken(res.data.accessToken) as TUser;
-      console.log(user);
+    //   const user = verifyToken(res.data.accessToken) as TUser;
+    //   console.log(user);
 
-      dispatch(setUser({ user: user, token: res.data.accessToken }));
-      toast.success("Logged In Successfully", { id: toastId, duration: 2000 });
-      navigate(`/${user.role}/dashboard`);
-    } catch (err) {
-      toast.error("Something went wrong", { id: toastId, duration: 2000 });
-    }
+    //   dispatch(setUser({ user: user, token: res.data.accessToken }));
+    //   toast.success("Logged In Successfully", { id: toastId, duration: 2000 });
+    //   navigate(`/${user.role}/dashboard`);
+    // } catch (err) {
+    //   toast.error("Something went wrong", { id: toastId, duration: 2000 });
+    // }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <PHForm onSubmit={onSubmit}>
       <div>
         <label htmlFor="id">ID:</label>
         <input type="text" id="id" {...register("userId")} />
@@ -53,7 +55,7 @@ const Login = () => {
         <input type="text" id="password" {...register("password")} />
       </div>
       <Button htmlType="submit">Login</Button>
-    </form>
+    </PHForm>
   );
 };
 
