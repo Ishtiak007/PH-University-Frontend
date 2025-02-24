b3ea1a3fecaba666b9a55071ecff61b64874d432
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-24 11:40:04 +0600

    add form for creating academic semester

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,5 +1,18 @@
+import { Button } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHInput from "../../../components/form/PHInput";
+import { FieldValues, SubmitHandler } from "react-hook-form";
+
 const CreateAcademicSemester = () => {
-  return <div></div>;
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    console.log(data);
+  };
+  return (
+    <PHForm onSubmit={onSubmit}>
+      <PHInput type="text" name="name" />
+      <Button htmlType="submit">Submit</Button>
+    </PHForm>
+  );
 };
 
 export default CreateAcademicSemester;
