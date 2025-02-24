037328128af15e13ee1eb71343915cf1e659d9ec
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-02-24 11:45:09 +0600

    update academic semester form with name and year fields

--- src/pages/admin/academicManagement/CreateAcademicSemester.tsx
+++ src/pages/admin/academicManagement/CreateAcademicSemester.tsx
@@ -1,4 +1,4 @@
-import { Button } from "antd";
+import { Button, Col, Flex } from "antd";
 import PHForm from "../../../components/form/PHForm";
 import PHInput from "../../../components/form/PHInput";
 import { FieldValues, SubmitHandler } from "react-hook-form";
@@ -8,10 +8,15 @@ const CreateAcademicSemester = () => {
     console.log(data);
   };
   return (
-    <PHForm onSubmit={onSubmit}>
-      <PHInput type="text" name="name" />
-      <Button htmlType="submit">Submit</Button>
-    </PHForm>
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHInput type="text" name="name" label="Name: " />
+          <PHInput type="text" name="name" label="Year: " />
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
   );
 };
 
