50783aa3b64b25ae50883265e516ca650ac7d9ab
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-01 15:24:50 +0600

    improve SemesterRegistration form with validation and proper option handling

--- src/pages/admin/courseManagement/SemesterRegistration.tsx
+++ src/pages/admin/courseManagement/SemesterRegistration.tsx
@@ -1,57 +1,70 @@
-import { Button, Col, Flex } from "antd";
-import PHForm from "../../../components/form/PHForm";
 import { FieldValues, SubmitHandler } from "react-hook-form";
+import PHForm from "../../../components/form/PHForm";
+import { Button, Col, Flex } from "antd";
 import PHSelect from "../../../components/form/PHSelect";
-import { semesterOptions } from "../../../constants/semester";
-import { monthOptions } from "../../../constants/global";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { academicSemesterSchema } from "../../../schemas/academicManagement.schema";
+
 import { toast } from "sonner";
 import { useGetAllSemestersQuery } from "../../../redux/features/admin/academicManagement.api";
+import PHDatePicker from "../../../components/form/PHDatePicker";
+import PHInput from "../../../components/form/PHInput";
+import { TResponse } from "../../../types";
+import { semesterStatusOptions } from "../../../constants/semester";
 
 const SemesterRegistration = () => {
-  const { data: academicSemester } = useGetAllSemestersQuery(undefined);
-  console.log("Data:", academicSemester);
-  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
-    const toastID = toast.loading("Creating");
+  const [addSemester] = useAddRegisteredSemesterMutation();
+  const { data: academicSemester } = useGetAllSemestersQuery([
+    { name: "sort", value: "year" },
+  ]);
 
-    const name = semesterOptions[Number(data?.name) - 1]?.label;
+  const academicSemesterOptions = academicSemester?.data?.map((item) => ({
+    value: item._id,
+    label: `${item.name} ${item.year}`,
+  }));
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const toastId = toast.loading("Creating...");
 
     const semesterData = {
-      name,
-      code: data.name,
-      year: data.year,
-      startMonth: data.startMonth,
-      endMonth: data.endMonth,
+      ...data,
+      minCredit: Number(data.minCredit),
+      maxCredit: Number(data.maxCredit),
     };
+
     console.log(semesterData);
-    // try {
-    //   console.log(semesterData);
-    //   const res = (await addAcademicSemester(semesterData)) as TResponse;
-    //   if (res.error) {
-    //     toast.error(res.error.data.message, { id: toastID });
-    //   } else {
-    //     toast.success("Semester is Created", { id: toastID });
-    //   }
-    // } catch (err) {
-    //   console.log(err);
-    //   toast.error("Something Went Wrong", { id: toastID });
-    // }
+
+    try {
+      const res = (await addSemester(semesterData)) as TResponse<any>;
+      console.log(res);
+      if (res.error) {
+        toast.error(res.error.data.message, { id: toastId });
+      } else {
+        toast.success("Semester created", { id: toastId });
+      }
+    } catch (err) {
+      toast.error("Something went wrong", { id: toastId });
+    }
   };
 
   return (
     <Flex justify="center" align="center">
       <Col span={6}>
-        <PHForm
-          onSubmit={onSubmit}
-          resolver={zodResolver(academicSemesterSchema)}
-        >
+        <PHForm onSubmit={onSubmit}>
           <PHSelect
-            name="startMonth"
-            label="Start Month"
-            options={monthOptions}
+            label="Academic Semester"
+            name="academicSemester"
+            options={academicSemesterOptions}
           />
-          <PHSelect name="endMonth" label="End Month" options={monthOptions} />
+
+          <PHSelect
+            name="status"
+            label="Status"
+            options={semesterStatusOptions}
+          />
+          <PHDatePicker name="startDate" label="Start Date" />
+          <PHDatePicker name="endDate" label="End Date" />
+          <PHInput type="text" name="minCredit" label="Min Credit" />
+          <PHInput type="text" name="maxCredit" label="Max Credit" />
+
           <Button htmlType="submit">Submit</Button>
         </PHForm>
       </Col>
