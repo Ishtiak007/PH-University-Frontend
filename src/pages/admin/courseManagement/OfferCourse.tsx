b53ef206c5073dfcfd57eb530057041cc2d79249
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-02 12:40:38 +0600

    Added OfferCourse component with academic semester selection and form handling

---
+++ src/pages/admin/courseManagement/OfferCourse.tsx
@@ -0,0 +1,44 @@
+import { Button, Col, Flex } from "antd";
+import PHForm from "../../../components/form/PHForm";
+import PHSelect from "../../../components/form/PHSelect";
+import PHDatePicker from "../../../components/form/PHDatePicker";
+import PHInput from "../../../components/form/PHInput";
+import { useGetAcademicDepartmentsQuery } from "../../../redux/features/admin/academicManagement.api";
+
+const OfferCourse = () => {
+  const { data: academicFacultyData } =
+    useGetAcademicDepartmentsQuery(undefined);
+
+  const academicSemesterOptions = academicFacultyData?.data?.map((item) => ({
+    value: item._id,
+    label: `${item.name}`,
+  }));
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <Flex justify="center" align="center">
+      <Col span={6}>
+        <PHForm onSubmit={onSubmit}>
+          <PHSelect
+            label="Academic Semester"
+            name="academicSemester"
+            options={academicSemesterOptions}
+          />
+
+          <PHSelect
+            name="status"
+            label="Status"
+            options={semesterStatusOptions}
+          />
+          <PHInput name="test" label="Test" type="text"></PHInput>
+
+          <Button htmlType="submit">Submit</Button>
+        </PHForm>
+      </Col>
+    </Flex>
+  );
+};
+
+export default OfferCourse;
