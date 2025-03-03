6c4a7cabd9bb3fb21e732f17b945213d35016c35
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2025-03-03 13:00:49 +0600

    add OfferedCourse component with API integration

--- src/pages/student/OfferedCourse.tsx
+++ src/pages/student/OfferedCourse.tsx
@@ -1,4 +1,8 @@
+import { useGetAllOfferedCoursesQuery } from "../../redux/features/student/studentCourseManagement.api";
+
 const OfferedCourse = () => {
+  const { data } = useGetAllOfferedCoursesQuery(undefined);
+  console.log(data);
   return <div>Offered Course component</div>;
 };
 
